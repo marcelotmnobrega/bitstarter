7a0b93d7dfaea2d453b27641773cd9b938a97c4e
Marcelo Nobrega <marcelotmnobrega@gmail.com>
2013-07-09 21:37:15 +0000

    modifying web.xml

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', 'utf-8'));
+  //response.send(fs.readFileSync('index.html', 'utf-8'));
+  response.send('Hello World from index.html');
   //response.send('Hello World2!');
 });
 
