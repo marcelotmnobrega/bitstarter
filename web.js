d146a4ab99336b689ae499ec9cdae1c8993f5b0a
Marcelo Nobrega <marcelotmnobrega@gmail.com>
2013-07-09 21:39:17 +0000

    using the fs library

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
-require('fs')
+var fs = require('fs')
 var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send(fs.readFileSync('index.html', 'utf-8'));
-  response.send('Hello World from index.html');
+  response.send(fs.readFileSync('index.html', 'utf-8'));
+  //response.send('Hello World from index.html');
   //response.send('Hello World2!');
 });
 
